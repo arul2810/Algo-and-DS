2d024a1da2a2df1eb12cb267ca00418b5dab22e2
arul2810 <arul2810@gmail.com>
2021-09-11 09:35:23 +0530

    Char Basics

--- Basic_DS/Dijkstra.cpp
+++ Basic_DS/Dijkstra.cpp
@@ -3,24 +3,44 @@
 using namespace std;
 
 
+class mindist_list{
+
+    public:
+
+    int distance;
+    int node_row;
+    int node_col;
+    mindist_list* next_node;
+
+};
+
+void update(){}
 
 
-
+void min_dist(){}
 
 int main(){
 
-    long m,n;
 
-    cin >> m >> n;
+    int row;
 
-    long array[m][n];
+    int col;
 
-    long dist_arr[m][n] = {0};
+    int matrix [row][col];
 
-    long path_arr [m][n] = {0};
+    cin >> row >> col;
 
+    for ( int i = 0 ; i < col ; i++){
 
+        for ( int j = 0 ; j < row ; j++){
 
+            cin >> matrix [i][j];
 
-    return 0;
+        }
+
+    }
+
+    // got thr matrix. time to cal min dist
+
+    for ( int )
 }

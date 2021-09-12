a304e51ac933d566d18566664d318e3e5c22578f
arul2810 <arul2810@gmail.com>
2021-09-12 22:02:19 +0530

    String_Substring

---
+++ HackerRank - Algo Implementation/Substring.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+void substring ( string input_string, long string_lenght){
+
+
+for ( int i = 0 ; i < string_lenght ; i ++ ){
+
+    for ( int j = 1 ; j <= string_lenght-i ; j ++ ){
+
+        cout << input_string.substr ( i, j ) << endl ;
+    }
+}
+
+
+
+
+}
+
+
+int main(){
+
+    string input_string;
+
+    cin >> input_string;
+
+    long string_length;
+
+    string_length =  input_string.length();
+
+    substring( input_string , string_length);
+
+
+    return 0;
+}
